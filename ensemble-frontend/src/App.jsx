a453ba2547ace97ffcec617e1b7d45b13c68cfc6
edmark <edmarktalingting0@gmail.com>
2026-03-24 22:23:07 +0800

    added oauth

--- ensemble-frontend/src/App.jsx
+++ ensemble-frontend/src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom'
+import LandingPage from './pages/LandingPage'
+import LoginPage from './pages/Auth/Loginpage'
+import SignupPage from './pages/Auth/Signuppage'
 import './App.css'
 
 function App() {
 
   return (
     <Routes>
-        <Route path="/" element={<><h1>Welcome to the App</h1></>} />
+        <Route path="/" element={<LandingPage />} />
+        <Route path='*' element={<h1>404 Not Found</h1>} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/signup' element={<SignupPage />} />
     </Routes>
   )
 }
