f294517bf52ed44c67e65eddd3fd78adeca90fe8
Jodeci <joedpacibe@gmail.com>
2026-06-05 03:30:02 +0800

    added preview indicator in audios tab, updated text and audio item ui in the timeline, disabled text selection, fixed textbox issues

--- video-editor/src/features/editor/timeline/items/audio.ts
+++ video-editor/src/features/editor/timeline/items/audio.ts
@@ -21,9 +21,7 @@ interface AudioProps extends TrimmableProps {
   trim: ITrim;
   duration: number;
   src: string;
-  metadata: Partial<IMetadata> & {
-    previewUrl: string;
-  };
+  metadata?: { name?: string; [key: string]: any };
 }
 
 class Audio extends Trimmable {
@@ -38,6 +36,8 @@ class Audio extends Trimmable {
   declare playbackRate: number;
   public bars: any[] = [];
 
+  public name: string = "Audio";
+
   static createControls(): { controls: Record<string, Control> } {
     return { controls: createAudioControls() };
   }
@@ -45,6 +45,7 @@ class Audio extends Trimmable {
   constructor(props: AudioProps) {
     super(props);
     this.id = props.id;
+    this.name = props.metadata?.name || "Audio";
     this.tScale = props.tScale;
     this.display = props.display;
     this.trim = props.trim;
@@ -146,21 +147,17 @@ class Audio extends Trimmable {
   }
 
   public drawTextIdentity(ctx: CanvasRenderingContext2D) {
-    const audioIconPath = new Path2D(
-      "M8.24092 0C8.24092 2.51565 10.2795 4.55419 12.7951 4.55419C12.9677 4.55419 13.1331 4.62274 13.2552 4.74475C13.3772 4.86676 13.4457 5.03224 13.4457 5.20479C13.4457 5.37734 13.3772 5.54282 13.2552 5.66483C13.1331 5.78685 12.9677 5.85539 12.7951 5.85539C11.9218 5.85605 11.0594 5.66105 10.2713 5.28471C9.48319 4.90838 8.78942 4.36027 8.24092 3.68066V13.8794C8.24094 14.8271 7.91431 15.7458 7.31606 16.4808C6.71781 17.2157 5.88451 17.722 4.95657 17.9143C4.02863 18.1066 3.06276 17.9731 2.22172 17.5364C1.38067 17.0997 0.715856 16.3865 0.339286 15.5169C-0.0372842 14.6473 -0.10259 13.6744 0.154372 12.7622C0.411334 11.8501 0.974857 11.0544 1.74999 10.5092C2.52512 9.96403 3.46449 9.7027 4.40981 9.76924C5.35512 9.83579 6.24861 10.2261 6.93972 10.8745V0H8.24092ZM6.93972 13.8794C6.93972 13.1317 6.6427 12.4146 6.11398 11.8859C5.58527 11.3572 4.86818 11.0602 4.12046 11.0602C3.37275 11.0602 2.65566 11.3572 2.12694 11.8859C1.59823 12.4146 1.3012 13.1317 1.3012 13.8794C1.3012 14.6272 1.59823 15.3443 2.12694 15.873C2.65566 16.4017 3.37275 16.6987 4.12046 16.6987C4.86818 16.6987 5.58527 16.4017 6.11398 15.873C6.6427 15.3443 6.93972 14.6272 6.93972 13.8794Z"
-    );
     ctx.save();
     ctx.translate(-this.width / 2, -this.height / 2);
-
-    ctx.translate(0, 6);
-    ctx.font = `400 12px ${SECONDARY_FONT}`;
-    ctx.fillStyle = "#ffffff";
-    ctx.textAlign = "left";
+    ctx.beginPath();
+    ctx.rect(0, 0, this.width, this.height);
     ctx.clip();
-    ctx.fillText("Audio", 36, 14);
-    ctx.translate(8, 1);
-    ctx.fillStyle = "#ffffff";
-    ctx.fill(audioIconPath);
+
+    ctx.font = `400 12px ${SECONDARY_FONT}`;
+    ctx.fillStyle = "rgba(255, 255, 255, 0.8)";
+    ctx.textAlign = "left";
+    ctx.fillText(this.name, 12, 22);
+
     ctx.restore();
   }
 
@@ -236,7 +233,7 @@ class Audio extends Trimmable {
     ctx.clip();
 
     // Draw waveform
-    ctx.fillStyle = "rgba(255, 255, 255, 0.5)";
+    ctx.fillStyle = "rgba(255, 255, 255, 0.4)";
     ctx.imageSmoothingEnabled = false;
 
     // Calculate which bars are visible
