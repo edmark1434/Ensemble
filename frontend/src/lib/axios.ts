4ce872edb2a9de890a003a84295ee6ce4e5ddf69
Edmark1434 <edmarktalingting0@gmail.com>
2026-07-02 01:20:23 +0800

    resolve pull axios file

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -4,6 +4,9 @@ import { API_BASE_URL } from "./api";
 const api = axios.create({
   baseURL: API_BASE_URL,
   withCredentials: true,
+  headers: {
+    "ngrok-skip-browser-warning": "true",
+  },
 });
 
 api.interceptors.response.use(
