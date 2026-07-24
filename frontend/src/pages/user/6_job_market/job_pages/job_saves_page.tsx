d4058c743d032d32c536efc8cfbcdf18a3537a1e
Rexshimura <mortpauljpm03@gmail.com>
2026-07-25 03:54:21 +0800

    revamped-job-market: My Saves Page - improved

--- frontend/src/pages/user/6_job_market/job_pages/job_saves_page.tsx
+++ frontend/src/pages/user/6_job_market/job_pages/job_saves_page.tsx
@@ -1,18 +1,19 @@
 import React from "react";
 import { useOutletContext, useParams } from "react-router-dom";
 import JobList from "../job_components/job_lists";
-import type {JobMainContext} from "../job_main";
+import type { JobMainContext } from "../job_main";
 
 const JobSavesPage: React.FC = () => {
-  const { jobsList, toggleSaveJob } = useOutletContext<JobMainContext>();
+  const { jobsList, loading, viewType, toggleSaveJob } = useOutletContext<JobMainContext>();
   const { id } = useParams();
 
-  // Filter down strictly to saved posts
   const savedJobs = jobsList.filter((job) => job.isSaved);
 
   return (
     <JobList
       jobs={savedJobs}
+      loading={loading}
+      viewType={viewType}
       activeJobId={id}
       onToggleSave={toggleSaveJob}
       baseRoute="/jobs/saved-posts"
