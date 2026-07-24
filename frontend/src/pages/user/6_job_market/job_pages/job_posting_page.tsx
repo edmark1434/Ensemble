c82f393f9d37da53108b8def976613ba0e0f6dff
Rexshimura <mortpauljpm03@gmail.com>
2026-07-25 03:54:35 +0800

    revamped-job-market: Job Postings Page - improved

--- frontend/src/pages/user/6_job_market/job_pages/job_posting_page.tsx
+++ frontend/src/pages/user/6_job_market/job_pages/job_posting_page.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { useOutletContext, useParams } from "react-router-dom";
 import JobList from "../job_components/job_lists";
-import type {JobMainContext} from "../job_main";
+import type { JobMainContext } from "../job_main";
 
 const JobPostingPage: React.FC = () => {
-  const { filteredJobs, toggleSaveJob } = useOutletContext<JobMainContext>();
+  const { filteredJobs, loading, viewType, toggleSaveJob } = useOutletContext<JobMainContext>();
   const { id } = useParams();
 
   return (
     <JobList
       jobs={filteredJobs}
+      loading={loading}
+      viewType={viewType}
       activeJobId={id}
       onToggleSave={toggleSaveJob}
       baseRoute="/jobs/postings"
