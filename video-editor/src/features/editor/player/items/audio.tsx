8610d9fa11d22baa16d5b47054bf9e99a52387af
Jodeci <joedpacibe@gmail.com>
2026-08-01 05:03:52 +0800

    fixed bug where mute state persists across sessions, upgraded remotion to 4.0.503, audio now uses html5audio

--- video-editor/src/features/editor/player/items/audio.tsx
+++ video-editor/src/features/editor/player/items/audio.tsx
@@ -1,6 +1,6 @@
 import { IAudio } from "@designcombo/types";
 import { BaseSequence, SequenceItemOptions } from "../base-sequence";
-import { Audio as RemotionAudio } from "remotion";
+import { Html5Audio } from "remotion";
 
 export default function Audio({
   item,
@@ -13,9 +13,9 @@ export default function Audio({
   const { details } = item;
   const playbackRate = item.playbackRate || 1;
   const children = (
-    <RemotionAudio
-      startFrom={(item.trim?.from! / 1000) * fps}
-      endAt={(item.trim?.to! / 1000) * fps || 1 / fps}
+    <Html5Audio
+      trimBefore={(item.trim?.from! / 1000) * fps}
+      trimAfter={(item.trim?.to! / 1000) * fps || 1 / fps}
       playbackRate={playbackRate}
       src={details.src}
       volume={() => (details.volume ?? 100) / 100}
