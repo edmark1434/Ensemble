7651988c4ac837e39ec204ea93e184c3a21670f1
Yohhannn <joehanes.lauglaug@ctu.edu.ph>
2026-08-23 01:29:09 +0800

    Corrected case sensitivity in import path for DashboardControllers

--- backend/routes/Dashboard.js
+++ backend/routes/Dashboard.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const dashboardController = require('../Controllers/DashboardControllers');
+const dashboardController = require('../controllers/DashboardControllers');
 const requireAuth = require('../middleware/requireAuth');
 
 router.use(requireAuth);
